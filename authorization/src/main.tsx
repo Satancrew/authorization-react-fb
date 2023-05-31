0cf88445b3b08caf0e7a77dbf5208537e65d89a8
Vladimir Orlov <86786994+Satancrew@users.noreply.github.com>
2023-05-31 18:06:47 +0300

    feat: add firebase settings (#2)

--- authorization/src/main.tsx
+++ authorization/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/index.ts';
 import App from './App.tsx';
+import './firebase.ts';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
